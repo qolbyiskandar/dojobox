789b6cd22f55c4a730a87366c3d644a106cd8535
qolbyiskandar <75964293+qolbyiskandar@users.noreply.github.com>
2021-08-04 14:56:19 +0700

    Delete test.js

--- Belajar JS/test.js
+++
@@ -1,4 +0,0 @@
-var bilangan1= parseInt(prompt('Masukkan bilangan kesatu: '));
-var bilangan2= parseInt(prompt('Masukkan bilangan kesatu: '));
-var hasil= parseInt(bilangan1+bilangan2);
-document.write('Hasil dari penjumlahan kedua bilangan tersebut adalah ' + hasil);
