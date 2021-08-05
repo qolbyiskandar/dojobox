f52c6a44338d56aa7ab52b69b3f58f44d9afdc93
Qolby Azizah <75964293+qolbyiskandar@users.noreply.github.com>
2021-08-06 02:01:38 +0700

    update 4 files, delete index.html

--- PostTest-Sprint1Automation/soalA.js
+++ PostTest-Sprint1Automation/soalA.js
@@ -6,3 +6,25 @@
 4. buat function untuk mengembalikan buku sesuai slotnya (yang kosong)
 5. buat function untuk mengganti buku yang ada
 */
+
+//membuat function untuk menambah 5 buku dengan judul bebas di rak
+alert('Selamat datang di Aplikasi Kelola Buku Perpustakaan')
+
+function tambahBuku(){
+    var buku=[];
+    var jmlSlotRakBuku=5;
+    var noBuku=1
+    while(noBuku<=jmlSlotRakBuku){
+        var tambah=prompt('Masukkan nama buku ke-' +noBuku+ ':')
+        document.write(noBuku + ". " + "Buku "+ '"' + tambah + '"' + " sudah ditambahkan ke rak buku"+'<br>')
+        buku.push(tambah);
+        noBuku++; 
+    }
+}
+
+tambahBuku()
+document.write(buku)
+
+function pinjam(){
+     
+}
--- PostTest-Sprint1Automation/soalB.js
+++ PostTest-Sprint1Automation/soalB.js
@@ -1,4 +1,11 @@
 //Program beli barang elektronik
 /* 
-1. 
+1. buat array untuk menyimpan data brg elektronik, isi dgn 5 jenis barnag
+2. terdiri dari: nama, jumlah, harga barang, dan discount
+3. bila nama barang yang diinput tidak ada dalam array akan muncul pesan
+4. discount berubah sesuai inputan
 */
+
+var dataBarang=[];
+document.write(dataBarang);
+document.write('<br>');
